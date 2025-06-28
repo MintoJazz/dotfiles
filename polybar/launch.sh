69fbdd0ce05e4700b60771e849fea20d107df545
MintoJazz <jinzo.matt@gmail.com>
2025-06-28 20:42:06 -0300

    Alugmas alteracoes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 
-# 1. Encerra instâncias de barras em execução
-killall -q polybar
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# 2. Aguarda até que os processos sejam finalizados
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log
+polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
-# 3. Lança a Polybar em cada monitor detectado
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    # A mágica acontece aqui: para cada monitor ($m), lança uma barra
-    # e passa o nome do monitor para a variável de ambiente MONITOR.
-    # Troque 'mybar' pelo nome da sua barra principal no config.ini
-    MONITOR=$m polybar --reload mybar &
-  done
-else
-  # Se xrandr não estiver disponível, lança uma única barra
-  polybar --reload mybar &
-fi
-
-echo "Polybar lançada!"
+echo "Bars launched..."
