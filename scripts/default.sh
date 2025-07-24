b6196eb348010be8727264acb84f6264cf4f727e
MintoJazz <jinzo.matt@gmail.com>
2025-07-24 18:11:49 -0300

    update24jul

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --mode 1440x900 --pos 0x0 --rotate normal --output DP-1 --primary --mode 1360x768 --pos 1440x0 --rotate normal --output HDMI-2 --off
+xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --primary --mode 1366x768 --pos 1366x0 --rotate normal --output HDMI-2 --off
